3fd929bf9f9d1cdbe08ed849ccb8c609c1b71e14
daud0095 <daudibragimov2004@gmail.com>
2026-02-19 15:31:41 +0100

    Task1

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+
+    public static void main(String[] args){
+
+        Student s1 = new Student("Arif", 22);
+        Student s2 = new Student("Loqman", 23);
+        Student s3 = new Student("Albert", 21);
+
+        Student[] students = {s1, s2, s3};
+
+        for(Student s : students){
+            s.printinfo();
+        }
+        /*
+        System.out.println("\nDen ældeste stundet");
+        for(Student s : students){
+            if(){
+
+            }
+        }
+         */
+
+    }
+
+
+}
---
+++ src/Student.java
@@ -0,0 +1,17 @@
+class Student {
+
+        String name;
+        int age;
+
+        Student(String name, int age){
+            this.name = name;
+            this.age = age;
+        }
+
+        void printinfo () {
+            System.out.println("Student name: " + name + " Stundent age: " + age);
+
+
+        }
+    }
+
